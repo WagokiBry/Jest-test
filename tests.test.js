372aeac2524cf0bc56033882cd44b8a4e1c84bab
Bryan <bryanwagoki2016@gmail.com>
2026-01-13 15:16:29 +0300

    style: format the document with prettier

--- tests.test.js
+++ tests.test.js
@@ -1,21 +1,29 @@
-import {sum} from './sum.js';
-import { capitalize } from './capitalise.js';
-import {reverse} from './reverse.js';
-import {createObject} from './calculator.js'
+import { sum } from "./sum.js";
+import { capitalize } from "./capitalise.js";
+import { reverse } from "./reverse.js";
+import { createObject } from "./calculator.js";
 
-test('adds 1 + 2 to equal 3', () => {
+test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test("takes a string and capitalize the first letter",()=>{
-  expect(capitalize("javascript")).toBe("Javascript")
-})
+test("takes a string and capitalize the first letter", () => {
+  expect(capitalize("javascript")).toBe("Javascript");
+});
 
-test("take a string and reverse it",()=>{
-  expect(reverse("hi")).toEqual("ih")
-})
+test("take a string and reverse it", () => {
+  expect(reverse("hi")).toEqual("ih");
+});
 
-test("calculator object returns an object",()=>{
-  const calc = new createObject()
-  expect(typeof calc).toEqual("object")
-})
+test("calculator object returns an object", () => {
+  const calc = new createObject();
+  expect(typeof calc).toEqual("object");
+});
+
+test("calculator objects has basic methods for calculation", () => {
+  let calc = new createObject();
+  expect(calc).toHaveProperty("add");
+  expect(calc).toHaveProperty("subtract");
+  expect(calc).toHaveProperty("multiply");
+  expect(calc).toHaveProperty("divide");
+});
