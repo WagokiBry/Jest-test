3f2d3ac99aa83e45a63006205245f547ba681eb8
Bryan <bryanwagoki2016@gmail.com>
2026-01-13 22:14:44 +0300

    refactor: update code to pass

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,9 +1,13 @@
 export function caesarCipher(str,shift){
     let result = "";
+    let base = "a".charCodeAt(0)
     for(let char of str){
-        result += String.fromCharCode(char.charCodeAt(0)+shift)
+        
+        result += String.fromCharCode((char.charCodeAt(0)+shift))
     }
     return result;
 }
+
+
 
 console.log(caesarCipher("xyz", 3))
